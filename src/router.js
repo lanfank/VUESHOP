e133762edb3f3e68b57f9a0331e07593ccf407ae
lanfank <lanfank8023@gmail.com>
2020-05-04 09:16:30 +0800

    登录功能补全

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Home from './components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     // 对初始化页面链接进行重定向
     { path: '/', redirect: '/login' },
@@ -14,3 +14,18 @@ export default new Router({
 
   ]
 })
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  // 接受当前的token值
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  return next()
+})
+// to代表将要访问的路径
+// from 代表从哪个路径跳转而来
+// next 是一个函数，表示放行
+// next() 放行  next('/login') 强制跳转
+
+export default router
