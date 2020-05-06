002a4184631ddf629aa4ab962f98a8129a35be42
lanfank <lanfank8023@gmail.com>
2020-05-06 17:32:55 +0800

    各模块初构建

--- src/router.js
+++ src/router.js
@@ -4,6 +4,13 @@ import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Welcome from './components/Welcome.vue'
 import Users from './components/user/Users.vue'
+import Rights from './components/power/Rights.vue'
+import Roles from './components/power/Roles.vue'
+import Goods from './components/goodmanager/Goods.vue'
+import Params from './components/goodmanager/Params.vue'
+import Categories from './components/goodmanager/Categories.vue'
+import Orders from './components/ordermanager/Orders.vue'
+import Reports from './components/statistic/Reports.vue'
 
 Vue.use(Router)
 
@@ -17,7 +24,14 @@ const router = new Router({
       component: Home,
       redirect: '/welcome',
       children: [{ path: '/welcome', component: Welcome },
-        { path: '/users', component: Users }
+        { path: '/users', component: Users },
+        { path: '/rights', component: Rights },
+        { path: '/roles', component: Roles },
+        { path: '/goods', component: Goods },
+        { path: '/params', component: Params },
+        { path: '/categories', component: Categories },
+        { path: '/orders', component: Orders },
+        { path: '/reports', component: Reports }
       ]
     }
 
