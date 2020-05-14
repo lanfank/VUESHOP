0e7af0b526d81c2df7d2eb36ab137aa104889aa0
lanfank <lanfank8023@gmail.com>
2020-05-14 13:42:41 +0800

    完成了数据报表的功能

--- src/components/statistic/Reports.vue
+++ src/components/statistic/Reports.vue
@@ -1,12 +1,81 @@
 <template>
     <div>
-        <h3>数据报表</h3>
+        <!-- 面包屑导航区域 -->
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+            <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片视图区域 -->
+        <el-card>
+            <!--为Echarts准备一个高宽大小的dom -->
+            <div id="main" style="width: 600px;height:400px;"></div>
+        </el-card>
     </div>
 </template>
 
 <script>
-export default {
+// 1、导入Echarts
+import echarts from 'echarts'
+import _ from 'lodash'
 
+export default {
+  data() {
+    return {
+      // 需要合并的数据
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  created() {
+
+  },
+  // 在元素渲染完成之后再开始加载echarts
+  async mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+    // 请求表格数据
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$msg.error('获取表格数据失败！')
+    }
+    console.log(res.data)
+    // 将数据项目和配置数据进行合并
+    const result = _.merge(this.options, res.data)
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result)
+  },
+  methods: {
+
+  }
 }
 </script>
 
